273a179013b5f5d83c917f5c671d6f65d9d6e715
rabidpraxis <kevin@kevinwebster.me>
2015-12-02 15:39:22 -0800

    loading rbenv

--- install.sh
+++ install.sh
@@ -4,7 +4,13 @@ command -v zsh >/dev/null 2>&1 || { echo >&2 "I require zsh but it's not install
 echo "* Downloading dotfiles"
 echo "* Loading submodule dependencies"
 git clone --quiet --recursive https://github.com/rabidpraxis/dotfiles.git
+
 cd dotfiles
+
+echo "* Installing ruby-build"
+mkdir -p utils/rbenv/plugins
+ln -s ../../ruby-build/ utils/rbenv/plugins/
+
 echo "* Installing ZSH deps"
 # Note: I have to inject this into the new zsh to keep this init script running
 ZDOTDIR=`pwd` zsh <<'EOF'
